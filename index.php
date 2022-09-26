7dfb3d6bb734962396784d29d6f7fcc6abf3a35f
christoli <christoliwonegou@gmail.com>
2022-09-26 12:18:45 +0000

    Home pagefrontend  commit

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
             <div class="row pt-5 pb-5 ">
                 <div class="col-md-6 col-sm-12 pt-5 pb-5">
                     <p class="mt-5">
-                        <span class="fs-4">La plateforme pour suivre et gérer votre santé</span><br/>
-                        <span class="fs-1">Prenez en main votre santé</span>
+                        <span class="fs-4">La plateforme pour suivre et gérer votre santé</span>
+                        <h2 class="fs-1">Prenez en main votre santé</h2>
                     </p>
                     <p class="fs-6 mt-3  pe-5">
                         Mayi Kondji vous permet de suivre vos consultations auprès des centres médico-sociaux et gérer vos données de santé en un seul endroit.
@@ -42,7 +42,39 @@
     </header>
     <main>
         <div class="container">
-            
+            <div class="row pt-5 pb-5 ">
+                <div class="col-md-6 col-sm-12 pt-5 pb-5 pe-5">
+                    <img src="assets/images/image_700x500.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-6 col-sm-12 pt-4 pb-5 ps-5">
+                    <p class="mt-5">
+                        <span class="fs-2">Mon espace santé</span>
+                    </p>
+                    <p class="fs-6 mt-3">
+                        Accédez aux informations concernant :
+                            <ul>
+                                <li>Vos consultations</li>
+                                <li>Vos analyses</li>
+                                <li>Vos traitements</li>
+                                <li>Vos suivis</li>
+                                <li>Et plus encore bientôt...</li>
+                            </ul>
+                    </p>
+
+                    <p class="mt-4">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet minus laboriosam, eius impedit odio libero laborum quasi.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center bg-light pt-5 pb-5">
+            <div class="col-md-4 text-center">
+                <h3 class="">Numériser son suivi médical ?</h3>
+                <p class="fs-6">Approchez-vous des professionels de santé ou de nos centres de santé pour plus d'informations.</p>
+                <p class="lead">
+                <a href="https://www.google.com/maps?q=centre+medico+sociaux+au+togo&sxsrf=ALiCzsYtQjiLe42cO-Gffq-iXSo30kMNzA:1664194359205&iflsig=AJiK0e8AAAAAYzGlRzlWQRCP7aeqPvynvTbHZPFwWDFc&gs_lcp=Cgdnd3Mtd2l6EAEYADIECCMQJzIECCMQJzIECCMQJzILCC4QgAQQxwEQrwEyBQgAEIAEMgUIABCABDIFCAAQgAQyCwguEIAEEMcBEK8BMgUIABCABDILCC4QgAQQxwEQrwE6CwguEIAEELEDEIMBOggIABCxAxCDAToICAAQgAQQsQM6EQguEIAEELEDEIMBEMcBEK8BOgUIABCxAzoLCAAQgAQQsQMQgwE6DgguEIAEELEDEMcBENEDOggILhCABBCxAzoFCC4QgAQ6CAguEIAEENQCUABY2AhguBhoAHAAeACAAagCiAHxCpIBAzItNpgBAKABAQ&um=1&ie=UTF-8&sa=X&ved=2ahUKEwig_Z2Qt7L6AhXTYPEDHeIeCQoQ_AUoBHoECAEQBg" target="_blank" class="btn btn-primary">Chercher</a>
+                </p>
+            </div>
         </div>
     </main>
     <footer>
